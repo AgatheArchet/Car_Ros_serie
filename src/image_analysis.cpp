71bc5ca16c1820286f601d85af404e9fa410dc4c
AgatheArchet <agathe.archet@ensta-breatgne.org>
2020-02-20 12:08:45 +0100

    Algo traitement d'image

--- src/image_analysis.cpp
+++ src/image_analysis.cpp
@@ -12,92 +12,70 @@
 
 using namespace std;
 
-/*
-The process is :
-*Capture an image
-*Define the ROI
-*Identify all of the black regions in this ROI
-*Select the left most or right most edge of the corresponding region
-*Offset this point to centre the tracking point
-*Calculate the steering signal based on how far the tracking point is from the centre of the ROI
 
-*/
+Mat pathDetection(Mat frame){
 
-/*
+    // Mat preparation for countours
+    Mat frame_gray(frame.size(), CV_8UC1), frame_opened;
+    cvtColor(frame, frame_gray, CV_RGB2GRAY);
+    threshold(frame_gray, frame_gray, 150, 255, CV_THRESH_BINARY);
+    erode(frame_gray, frame_opened, getStructuringElement(MORPH_RECT, Size(2, 2)));
+    dilate(frame_opened, frame_opened, getStructuringElement(MORPH_RECT, Size(8, 8)));
 
-Mat video(VideoCapture cap){
-    if(!cap.open(0)){
-        cout << "Error opening video stream or file" << endl;
-        break;
-    }
-    int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
-    int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-    //VideoWriter video("../images/webcam.avi",CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height),true);
-    Mat frame;
-    cap >> frame;
-    if (frame.empty())
-      break;
-    return frame;
-} */
-
-float getMidPoint(int bias, Mat camImage)
-{   
-    //video(VideoCapture cap);
-    Rect region_of_interest = Rect(10, 2*camImage.rows/3, camImage.cols-20, camImage.rows/12);
-    Mat roi = camImage(region_of_interest);
-    Mat mono;
-    cvtColor(roi, mono, COLOR_RGB2GRAY);
-    Mat blur;
-    GaussianBlur(mono, blur, Size(9, 9), 2, 2);
-    Mat thresh;
-    threshold(blur, thresh, 0, 255, THRESH_BINARY_INV|THRESH_OTSU);
-    Mat dilateImg;
-    dilate(thresh, dilateImg, getStructuringElement(MORPH_RECT, Size(5, 5)));
-    Mat erodeImg;
-    erode(dilateImg, erodeImg, getStructuringElement(MORPH_RECT, Size(5, 5)));
+    // Find countours
     vector<vector<Point> > contours;
-    vector<Vec4i> notused;
-    findContours(erodeImg, contours, notused, RETR_LIST, CHAIN_APPROX_SIMPLE);
-    double minMaxCx = (bias > 0 ? -std::numeric_limits<double>::infinity() : std::numeric_limits<double>::infinity());
-    for(int i = 0; i< contours.size(); i++ )
-    {
-        Moments mu = moments(contours, false);
-        if (mu.m00 > 100.0)
-        {
-            Rect r = boundingRect(contours);
-            double cx;
-            if (bias > 0)
-            {
-                cx = r.x + r.width - 12;
-                if (cx > minMaxCx)
-                {
-                    minMaxCx = cx;
-                }
-            }
-            else
-            {
-                cx = r.x + 12;
-                if (minMaxCx > cx)
-                {
-                    minMaxCx = cx;
-                }
-            }
-        }
-    }
-    if (isinf(minMaxCx))
-        minMaxCx = roi.cols/2;
-    
-    return 1.0f - 2.0f*(float)minMaxCx/roi.cols;
-}
+    vector<Vec4i> hierarchy;
+    RNG rng(12345);
+    findContours( frame_opened, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
+    /// Get rotated rectangle that best fits the countours
+    vector<RotatedRect> minRect( contours.size() );
+
+    for( int i = 0; i < contours.size(); i++ )
+     { 
+        minRect[i] = minAreaRect( Mat(contours[i]) );
+     }
+    
+    /// Draw countours + rotated rectangle 
+    Mat drawing = Mat::zeros( frame_opened.size(), CV_8UC3 );
+
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        Scalar white = Scalar( 255,255,255 );
+        Scalar red = Scalar( 0,0,255 );
+        // countours
+        drawContours( drawing, contours, i, white, 2, 8, hierarchy, 0, Point() );
+        // rotated rectangle
+        Point2f rect_points[4]; minRect[i].points( rect_points );
+        for( int j = 0; j < 4; j++ )
+        {
+            line( drawing, rect_points[j], rect_points[(j+1)%4], red, 2, 8 );
+        }
+    } 
+
+    // Draw directional line 
+    Mat line;
+    vector<Point> cnt = contours.at(0);
+    int rows =drawing.rows, cols = drawing.cols;
+    fitLine(cnt,line, CV_DIST_L2,0,0.01,0.01);
+    float vx = line.at<float>(0,0),vy = line.at<float>(1,0), x = line.at<float>(2,0), y = line.at<float>(3,0);
+    float lefty = int((-x*vy/vx) + y);
+    float righty = int(((cols-x)*vy/vx)+y);
+    Scalar green = Scalar( 0,255,0 );
+    cv::line(drawing,Point(cols-1,righty),Point(0,lefty),green,2,8);
+
+    return drawing;
+}
 
 
 
 int main(int argc, char **argv){
 
-    Mat I = imgUtils::readImage(argc, argv);
-    float midpoint = getMidPoint(0, I);
-    cout << midpoint << endl;
+    Mat Image = cv::imread(argv[1], 1); //loads color if it is available
+    Mat PathImage = pathDetection(Image);
+    imshow("Contours",PathImage);
+
+    waitKey(0);
     
     return 0;
 }
